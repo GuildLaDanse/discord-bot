51598e5ef0da587a803fe544f785a98b61bc5959
Bavo De Ridder <bavo@coderspotting.org>
2017-11-11 22:06:10 +0100

    Initial revision

---
+++ LaDanseDiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaDanseDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
