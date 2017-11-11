5fdcb07009067b4d8a6d8c714924b6da2a21d8bd
Bavo De Ridder <bavo@coderspotting.org>
2017-11-11 22:26:01 +0100

    Initial revision

--- LaDanseDiscordBot/Program.cs
+++ LaDanseDiscordBot/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LaDanseDiscordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+        
+        private IConfigurationRoot _config;
+
+        public async Task StartAsync()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()    // Begin building the configuration file
+                .SetBasePath(AppContext.BaseDirectory)  // Specify the location of the config
+                .AddJsonFile("configuration.json");     // Add the configuration file
+            _config = builder.Build();                  // Build the configuration file
+
+            var services = new ServiceCollection()      // Begin building the service provider
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig     // Add the discord client to the service provider
+                {
+                    LogLevel = LogSeverity.Verbose,
+                    MessageCacheSize = 1000     // Tell Discord.Net to cache 1000 messages per channel
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig     // Add the command service to the service provider
+                {
+                    DefaultRunMode = RunMode.Async,     // Force all commands to run async
+                    LogLevel = LogSeverity.Verbose
+                }))
+                //.AddSingleton<CommandHandler>()     // Add remaining services to the provider
+                //.AddSingleton<LoggingService>()     
+                //.AddSingleton<StartupService>()
+                .AddSingleton<Random>()             
+                .AddSingleton(_config);
+
+            var provider = services.BuildServiceProvider();     // Create the service provider
+
+            //provider.GetRequiredService<LoggingService>();      // Initialize the logging service, startup service, and command handler
+            //await provider.GetRequiredService<StartupService>().StartAsync();
+            //provider.GetRequiredService<CommandHandler>();
+
+            Console.WriteLine("Starting La Danse Discord Bot");
+            
+            await Task.Delay(-1);     // Prevent the application from closing
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # discord-bot
 A bot to connect the La Danse website with the Discord server
+
+Initial code is based on https://github.com/Aux/Discord.Net-Example
